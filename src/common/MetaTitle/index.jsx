356c254beeebd8a0753a1565d6334d6055a32997
Veeravel-S <veeravel.igt@gmail.com>
2025-11-28 20:00:55 +0530

    meta title added

---
+++ src/common/MetaTitle/index.jsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import React from "react";
+
+export const MetaTitle = ({ title, description, keywords }) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+    </Head>
+  );
+};
